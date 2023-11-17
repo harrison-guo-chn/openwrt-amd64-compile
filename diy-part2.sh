02c34fbed5f2467a8985336dd4be9da2ab7b89d8
harrison-guo-chn <58265745+harrison-guo-chn@users.noreply.github.com>
2023-11-17 22:41:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
 sed -i 's/OpenWrt/CncTion-N5105-4L/g' package/base-files/files/bin/config_generate
